ccf01fef5c6ca9fd7b8f59a13a85cb6069375c4b
Filipe <filipemagalha3s@gmail.com>
2023-04-24 15:53:21 -0300

    entrada e saida: geral 14

--- Entrada e Saída/geral_14.c
+++ Entrada e Saída/geral_14.c
@@ -9,9 +9,9 @@ int main()
     scanf("%d %d", &valor_sobre15, &percentual_disc);
 
     preco_base = valor_sobre15 * 15 + percentual_disc;
-    preco_disc = preco_base - 0.14*preco_base;
+    preco_disc = preco_base - 0.14 * preco_base;
 
     printf("Valor a pagar:\tR$ %.2f ", preco_disc);
 
-        return 0;
+    return 0;
 }
